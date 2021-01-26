7093ed3b171da1e7bdbbc4630ec3a9d46a621d88
Jregus <69990337+Jregus@users.noreply.github.com>
2021-01-26 14:02:32 -0700

    hw changes

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $("#item1").hover(function(){
+        $(this).css("background-color", "#cccccc");
+    },
+      function(){
+        $(this).css("background-color", "white");
+
+    })
+    $("#item2").hover(function(){
+        $(this).css("background-color", "#cccccc");
+    },
+      function(){
+        $(this).css("background-color", "white");
+
+    })
+    $("#item3").hover(function(){
+        $(this).css("background-color", "#cccccc");
+    },
+      function(){
+        $(this).css("background-color", "white");
+
+    })
+});
